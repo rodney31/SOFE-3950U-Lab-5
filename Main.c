3b59a37dffa0836198c60504563347772e822d8f
rodney31 <67178645+rodney31@users.noreply.github.com>
2024-04-04 18:30:56 -0400

    Update Main.c
    Some more starting structure of program

--- Main.c
+++ Main.c
@@ -6,20 +6,80 @@
 #include <signal.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <stdbool.h>
+#include <pthread.h>
 
 /* these may be any values >= 0 */
-#define NUMBER OF CUSTOMERS 5
-#define NUMBER OF RESOURCES 3
-/* the available amount of each resource */
-int available[NUMBER OF RESOURCES];
-/*the maximum demand of each customer */
-int maximum[NUMBER OF CUSTOMERS][NUMBER OF RESOURCES];
-/* the amount currently allocated to each customer */
-int allocation[NUMBER OF CUSTOMERS][NUMBER OF RESOURCES];
-/* the remaining need of each customer */
-int need[NUMBER OF CUSTOMERS][NUMBER OF RESOURCES];
+#define NUMBER_OF_CUSTOMERS 5
+#define NUMBER_OF_RESOURCES 3
 
-int main(void)
+void resources(int customerNum); 
+void acquire(lock *mutex);
+void acquire(lock *mutex);
+int requestResources(int customerNum, int request[]);
+int releaseResources(int customerNum, int release[]);
+
+typedef struct {
+    bool available;
+} lock;
+
+int main(int argc, char **argv)
 { 
+    srand(time(NULL)); 
+    
+    int available[NUMBER_OF_RESOURCES] = {argv[1] - '0', argv[2] - '0', argv[3] - '0', argv[4] - '0'}; /* the available amount of each resource */
+    int maximum[NUMBER_OF_CUSTOMERS][NUMBER_OF_RESOURCES]; 
+    int randomNumber;
 
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            randomNumber = (rand() % 10) + 2;
+
+            maximum[i][j] = randomNumber; 
+        }
+    }
+    
+    int allocation[NUMBER_OF_CUSTOMERS][NUMBER_OF_RESOURCES]; 
+    int need[NUMBER_OF_CUSTOMERS][NUMBER_OF_RESOURCES]; 
+    
+    pthread_t tid[5];
+    pthread_attr_t attr[5];
+
+    for (int i = 0; i < 5 ;i++)
+    {
+        pthread_attr_init(&attr[i]);
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        pthread_create(&tid[i], &attr[i], resources, i);
+    }
+}
+
+public void resources(int customerNum) 
+{
+    mutex->available = true; 
+    
+    do{
+        acquire(mutex);
+
+        // Requesting and Releasing logic, including calls to functions, go between here 
+
+        release(mutex);
+        
+    } while(true);
+}
+
+void acquire(lock *mutex)
+{
+    while (!mutex->available);
+    
+    mutex->available = false;
+}
+
+void release(lock *mutex)
+{
+    mutex->available = true; 
 }
